073754db9024e8373876ff35b4bf52f0f107233d
Jake Miller <jakemiller.bc@gmail.com>
2022-05-24 20:02:06 -0500

    commit

---
+++ fundementals/fundementals/Miller_Jake_ForLoopBasic1/for_loop_basic1.py
@@ -0,0 +1,38 @@
+# x = 0
+# for x in range(151):
+#     print(x)
+
+# i = 5
+# for i in range(5,1001):
+#     if i % 5 == 0:
+#         print(i)
+
+# x = 1
+# for x in range(1,100):
+#     if x % 10 == 0:
+#         print('Coding Dojo')
+#     elif x % 5 == 0:
+#         print('Coding')
+#     else:
+#         print(x)
+
+# total = 0
+# oddlist = []
+# for t in range(500000):
+#     if t % 2 != 0:
+#         oddlist.append(t)
+
+# Sum = sum(oddlist)
+# print(Sum)
+
+# for f in range(2018,0,-4):
+#     print(f)
+
+lowNum = 5
+highNum =20
+mult = 4
+for a in range(lowNum,highNum):
+    if a % int(mult) == 0:
+        print(a)
+
+
